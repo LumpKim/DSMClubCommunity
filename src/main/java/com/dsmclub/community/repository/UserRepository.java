c82788adef09e17cd0220c4b68e6dde3c2032655
by09115 <by09115@outlook.kr>
2019-05-10 15:00:51 +0900

    [CREATE] JPA 사용을 위한 클래스, Repository 추가

---
+++ src/main/java/com/dsmclub/community/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.dsmclub.community.repository;
+
+import com.dsmclub.community.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
